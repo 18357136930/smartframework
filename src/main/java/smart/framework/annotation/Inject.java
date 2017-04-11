3e829310885cff50d1f4f1e7837a6564bfcbd5e5
18357136930 <18357136930@16.com>
2017-04-11 16:02:37 +0800

    添加自定义注解

---
+++ src/main/java/smart/framework/annotation/Inject.java
@@ -0,0 +1,14 @@
+package smart.framework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inject {
+}
